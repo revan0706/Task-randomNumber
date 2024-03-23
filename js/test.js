61a8f5c12f416d6497bd5ea60412875592d991fc
revan <rvanliyev55@gmail.com>
2024-03-23 04:17:06 +0400

    Task complete

---
+++ js/test.js
@@ -0,0 +1,11 @@
+function randomNum() {
+  let a = document.querySelectorAll("h1");
+  for (let i = 0; i < 3; i++) {
+    let random = Math.floor(Math.random() * 10);
+    a[i].innerHTML = random;
+  }
+}
+document.querySelector("div").style =
+  "display : flex ; justify-content: space-evenly";
+document.querySelector("input").style =
+  "position : absolute ; top:50% ; left:50% ; padding:16px 29px;font-size:36px;border-radius:8px;background-color:black ; color:white;";
